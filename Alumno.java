27a2b0c267d426dd0903f2739a751ec4d2c2b22e
Hugopereezzz <hugopereezzzz@gmail.com>
2025-10-17 00:07:29 +0200

    HilosAlumno

---
+++ Alumno.java
@@ -0,0 +1,25 @@
+package HiloAlumnos;
+
+public class Alumno implements Runnable{
+
+    String nombre;
+
+    public Alumno (String nombre){
+        this.nombre=nombre;
+    }
+
+
+        @Override
+    public void run() {
+        for (int i = 1; i < 6; i++) {
+            System.out.println("Hola soy " + nombre + " y este es mi mensaje numero " + i);
+
+        try {
+            Thread.sleep(1500);
+            }   catch (InterruptedException e) {
+                System.out.println("El hilo fue interrumpido");
+                }
+        }
+    }
+
+}
